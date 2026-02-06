f9047cc6e5ceffb3e19ffbeb30cf4a3de8638e35
Data Wizual <121548721+DataWizual@users.noreply.github.com>
2026-02-06 12:01:54 +0500

    Delete test_infra.tf

--- test_infra.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_security_group" "bad_example" {
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Это вызовет срабатывание правила INFRA-002
-  }
-}
